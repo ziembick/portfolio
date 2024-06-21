ecb382955b885df4cb22a82e09e43b4d1b61ad9e
Paulo Ziembick <pauloziembick@gmail.com>
2024-06-20 23:56:25 -0300

    kakakaka

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,5 +1,8 @@
-import { NextResponse } from 'next/server';
-import { z } from 'zod';
+
+
+import axios from "axios";
+import { NextResponse } from "next/server";
+import { z } from "zod";
 
 const bodySchema = z.object({
   name: z.string(),
@@ -7,56 +10,51 @@ const bodySchema = z.object({
   message: z.string(),
 });
 
-const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY!;
-const SENDGRID_SENDER_EMAIL = 'pauloziembick@gmail.com';
+const WEBHOOK_URL = process.env.WEBHOOK_URL!;
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { name, email, message } = bodySchema.parse(body);
 
-    const emailData = {
-      personalizations: [
+    const messageData = {
+      embeds: [
         {
-          to: [{ email: SENDGRID_SENDER_EMAIL }],
-          subject: `New message from ${name}`,
-        },
-      ],
-      from: { email },
-      content: [
-        {
-          type: 'text/plain',
-          value: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
+          title: `Mensagem Contato`,
+          color: 0x4984f5,
+          fields: [
+            {
+              name: `Nome`,
+              value: name,
+              inline: true,
+            },
+            {
+              name: `Email:`,
+              value: email,
+              inline: true,
+            },
+            {
+              name: `Mensagem:`,
+              value: message,
+            },
+          ],
         },
       ],
     };
 
-    const response = await fetch('https://api.sendgrid.com/v3/mail/send', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${SENDGRID_API_KEY}`,
-      },
-      body: JSON.stringify(emailData),
-    });
+    await axios.post(WEBHOOK_URL, messageData)
 
-    if (response.ok) {
-      return NextResponse.json({ message: 'Mensagem enviada com sucesso!' });
-    } else {
-      const error = await response.json();
-      console.error('SendGrid error:', error);
-      return NextResponse.error();
-    }
+    return NextResponse.json({
+        message: 'Mensagem enviada com sucesso!'
+    })
   } catch (err) {
     console.error(err);
     return NextResponse.error();
   }
 }
 
-
-// import axios from "axios";
-// import { NextResponse } from "next/server";
-// import { z } from "zod";
+// import { NextResponse } from 'next/server';
+// import { z } from 'zod';
 
 // const bodySchema = z.object({
 //   name: z.string(),
@@ -64,45 +62,49 @@ export async function POST(request: Request) {
 //   message: z.string(),
 // });
 
-// const WEBHOOK_URL = process.env.WEBHOOK_URL!;
+// const SENDGRID_API_KEY = process.env.SENDGRID_API_KEY!;
+// const SENDGRID_SENDER_EMAIL = 'pauloziembick@gmail.com';
 
 // export async function POST(request: Request) {
 //   try {
 //     const body = await request.json();
 //     const { name, email, message } = bodySchema.parse(body);
 
-//     const messageData = {
-//       embeds: [
+//     const emailData = {
+//       personalizations: [
 //         {
-//           title: `Mensagem Contato`,
-//           color: 0x4984f5,
-//           fields: [
-//             {
-//               name: `Nome`,
-//               value: name,
-//               inline: true,
-//             },
-//             {
-//               name: `Email:`,
-//               value: email,
-//               inline: true,
-//             },
-//             {
-//               name: `Mensagem:`,
-//               value: message,
-//             },
-//           ],
+//           to: [{ email: SENDGRID_SENDER_EMAIL }],
+//           subject: `New message from ${name}`,
+//         },
+//       ],
+//       from: { email },
+//       content: [
+//         {
+//           type: 'text/plain',
+//           value: `Name: ${name}\nEmail: ${email}\nMessage: ${message}`,
 //         },
 //       ],
 //     };
 
-//     await axios.post(WEBHOOK_URL, messageData)
+//     const response = await fetch('https://api.sendgrid.com/v3/mail/send', {
+//       method: 'POST',
+//       headers: {
+//         'Content-Type': 'application/json',
+//         Authorization: `Bearer ${SENDGRID_API_KEY}`,
+//       },
+//       body: JSON.stringify(emailData),
+//     });
 
-//     return NextResponse.json({
-//         message: 'Mensagem enviada com sucesso!'
-//     })
+//     if (response.ok) {
+//       return NextResponse.json({ message: 'Mensagem enviada com sucesso!' });
+//     } else {
+//       const error = await response.json();
+//       console.error('SendGrid error:', error);
+//       return NextResponse.error();
+//     }
 //   } catch (err) {
 //     console.error(err);
 //     return NextResponse.error();
 //   }
 // }
+
